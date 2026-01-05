2ee64c3623a1922e0d37bfdb9309785b275f8d17
Aneeja55 <aneeja5505@gmail.com>
2026-01-05 22:21:59 +0530

    face extraction working in my lap

---
+++ Face landmarks detection.py
@@ -0,0 +1,5 @@
+import cv2
+import mediapipe as mp
+image=cv2.imread("frame30.jpg")
+cv2.imshow("Input Image",image)
+cv2.waitKey(0)
---
+++ READme.md
@@ -0,0 +1,3 @@
+Dependencies required:
+    1. pip install python
+    2. pip install mediapipe
